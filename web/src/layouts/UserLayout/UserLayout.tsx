72fec14cc847c8eb8b535b76605841d0f080df4e
yeabu369 <yeabsdebe@gmail.com>
2022-09-05 10:15:40 +0000

    refactor: Changed main to box

--- web/src/layouts/UserLayout/UserLayout.tsx
+++ web/src/layouts/UserLayout/UserLayout.tsx
@@ -1,9 +1,17 @@
+import { Box } from '@chakra-ui/react'
+
 type UserLayoutProps = {
   children?: React.ReactNode
 }
 
 const UserLayout = ({ children }: UserLayoutProps) => {
-  return <main>{children}</main>
+  return (
+    <>
+      <Box as="main" borderWidth="1px" p={4}>
+        {children}
+      </Box>
+    </>
+  )
 }
 
 export default UserLayout
